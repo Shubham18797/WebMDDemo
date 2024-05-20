2dfef040077ea6bbb12193962c83a8c8725ec82e
Shubhamac18 <shubhamchavan@monocept.com>
2024-05-20 12:41:21 +0530

    add loader

--- WebMDInterviewTestSwiftUI/View/NewsListView.swift
+++ WebMDInterviewTestSwiftUI/View/NewsListView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct NewsListView: View {
     
     @ObservedObject var viewModel: NewsFeedViewModel
+    @State private var hasLoadedNews = false
     
     var body: some View {
         NavigationView {
@@ -17,27 +18,41 @@ struct NewsListView: View {
             // 2. Each article cell should show an image,title, and description.
             // 3. Implement navigation to a detail view on selection.
             
-            List(viewModel.newsArr, id: \.id) { news in
+            Group {
                 
-                NavigationLink(destination: NewsArticleView(news: news)) {
-                    HStack {
-                        VStack {
-                            Text(news.title ?? "").font(.title3).fontWeight(.bold).lineLimit(2)
-                            Spacer().frame(height: 10)
-                            Text(news.description ?? "").font(.subheadline).lineLimit(3)
+                if viewModel.isLoading {
+                    ProgressView("Fetching news...")
+                        .progressViewStyle(.circular)
+                    
+                }
+                else {
+                    List(viewModel.newsArr, id: \.id) { news in
+                        
+                        NavigationLink(destination: NewsArticleView(news: news)) {
+                            HStack {
+                                VStack {
+                                    Text(news.title ?? "").font(.title3).fontWeight(.bold).lineLimit(2)
+                                    Spacer().frame(height: 10)
+                                    Text(news.description ?? "").font(.subheadline).lineLimit(3)
+                                }
+                                Spacer()
+                                
+                                LazyImageView(imageUrl: news.urlToImage ?? "", width: 100, height: 100)
+                                
+                            }
                         }
-                        Spacer()
                         
-                        LazyImageView(imageUrl: news.urlToImage ?? "", width: 100, height: 100)
-                        
-                    }
+                    }.listStyle(.plain)
+                        .navigationTitle("News")
+                        .onAppear(perform: {
+                            if !hasLoadedNews {
+                                viewModel.loadNewsFeed()
+                                hasLoadedNews.toggle()
+                            }
+                        })
                 }
                 
-            }.listStyle(.plain)
-                .navigationTitle("News")
-                .onAppear(perform: {
-                    viewModel.loadNewsFeed()
-                })
+            }
         }
     }
 }
--- WebMDInterviewTestSwiftUI/ViewModel/NewsFeedViewModel.swift
+++ WebMDInterviewTestSwiftUI/ViewModel/NewsFeedViewModel.swift
@@ -11,14 +11,19 @@ import Combine
 class NewsFeedViewModel: ObservableObject {
     
     @Published var newsArr: [NewsArticle] = []
+    @Published var isLoading: Bool = false
     
     // Function to load news feed
     func loadNewsFeed() {
-        // fetchNews and handle its response
-        // Please show only the first 25 articles
-        
+//        // fetchNews and handle its response
+//        // Please show only the first 25 articles
+                
+        print("Started loading news feed.")
+        isLoading = true
         NewsAPI.fetchNews {[weak self] result in
             DispatchQueue.main.async {
+                self?.isLoading = false
+                print("Finished loading news feed.")
                 switch result {
                 case .success(let arr):
                     self?.newsArr = Array(arr.prefix(25))
